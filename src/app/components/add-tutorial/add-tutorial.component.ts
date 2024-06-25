0507242b76029883be2b6a93e9d0f47d0ff80b36
adsoftsito <adsoft@live.com.mx>
2024-06-25 14:03:34 -0600

    add r1

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -24,4 +24,158 @@ export class AddTutorialComponent {
     this.submitted = false;
     this.tutorial = new Tutorial();
   }
+
+  setM1(value: Number): Number {
+    
+    this.tutorial.persuasivoM = 0;
+    this.tutorial.gentilM =0;
+    this.tutorial.humildeM =0;
+    this.tutorial.originalM =0;
+
+    if  (value==0) 
+      this.tutorial.persuasivoM = 1;
+    if  (value==1) 
+      this.tutorial.gentilM = 1;
+    if  (value==2) 
+      this.tutorial.humildeM = 1;
+    if  (value==3) 
+      this.tutorial.originalM = 1;
+
+    return 1;
+  }
+  setL1(value: Number): void {
+
+    this.tutorial.persuasivoL = 0;
+    this.tutorial.gentilL =0;
+    this.tutorial.humildeL =0;
+    this.tutorial.originalL =0;
+
+    if  (value==0) 
+      this.tutorial.persuasivoL = 1;
+    if  (value==1) 
+      this.tutorial.gentilL = 1;
+    if  (value==2) 
+      this.tutorial.humildeL = 1;
+    if  (value==3) 
+      this.tutorial.originalL = 1;
+
+
+  }
+
+
+  setM2(value: Number): Number {
+    
+    this.tutorial.fuerzavoluntadM = 0;
+    this.tutorial.menteabiertaM =0;
+    this.tutorial.complacienteM =0;
+    this.tutorial.animosoM =0;
+
+    if  (value==0) 
+      this.tutorial.fuerzavoluntadM = 1;
+    if  (value==1) 
+      this.tutorial.menteabiertaM = 1;
+    if  (value==2) 
+      this.tutorial.complacienteM = 1;
+    if  (value==3) 
+      this.tutorial.animosoM = 1;
+
+    return 1;
+  }
+
+  setL2(value: Number): Number {
+    
+    this.tutorial.fuerzavoluntadL = 0;
+    this.tutorial.menteabiertaL =0;
+    this.tutorial.complacienteL =0;
+    this.tutorial.animosoL =0;
+
+    if  (value==0) 
+      this.tutorial.fuerzavoluntadL = 1;
+    if  (value==1) 
+      this.tutorial.menteabiertaL = 1;
+    if  (value==2) 
+      this.tutorial.complacienteL = 1;
+    if  (value==3) 
+      this.tutorial.animosoL = 1;
+
+    return 1;
+  }
+
+  setM3(value: Number): Number {
+    
+    this.tutorial.obedienteM = 0;
+    this.tutorial.quisquillosoM =0;
+    this.tutorial.inconquistableM =0;
+    this.tutorial.juguetonM =0;
+
+    if  (value==0) 
+      this.tutorial.obedienteM = 1;
+    if  (value==1) 
+      this.tutorial.quisquillosoM = 1;
+    if  (value==2) 
+      this.tutorial.inconquistableM = 1;
+    if  (value==3) 
+      this.tutorial.juguetonM = 1;
+
+    return 1;
+  }
+
+  setL3(value: Number): Number {
+    
+    this.tutorial.obedienteL = 0;
+    this.tutorial.quisquillosoL =0;
+    this.tutorial.inconquistableL =0;
+    this.tutorial.juguetonL =0;
+
+    if  (value==0) 
+      this.tutorial.obedienteL = 1;
+    if  (value==1) 
+      this.tutorial.quisquillosoL = 1;
+    if  (value==2) 
+      this.tutorial.inconquistableL = 1;
+    if  (value==3) 
+      this.tutorial.juguetonL = 1;
+
+    return 1;
+  }
+
+  setM4(value: Number): Number {
+    
+    this.tutorial.aventureroM = 0;
+    this.tutorial.receptivoM =0;
+    this.tutorial.cordialM =0;
+    this.tutorial.moderadoM =0;
+
+    if  (value==0) 
+      this.tutorial.aventureroM = 1;
+    if  (value==1) 
+      this.tutorial.receptivoM = 1;
+    if  (value==2) 
+      this.tutorial.cordialM = 1;
+    if  (value==3) 
+      this.tutorial.moderadoM = 1;
+
+    return 1;
+  }
+
+  setL4(value: Number): Number {
+    
+    this.tutorial.aventureroL = 0;
+    this.tutorial.receptivoL =0;
+    this.tutorial.cordialL =0;
+    this.tutorial.moderadoL =0;
+
+    if  (value==0) 
+      this.tutorial.aventureroL = 1;
+    if  (value==1) 
+      this.tutorial.receptivoL = 1;
+    if  (value==2) 
+      this.tutorial.cordialL = 1;
+    if  (value==3) 
+      this.tutorial.moderadoL = 1;
+
+    return 1;
+  }
+
+
 }
--- src/app/models/tutorial.model.ts
+++ src/app/models/tutorial.model.ts
@@ -1,7 +1,47 @@
 export class Tutorial {
   id?: string;
+
+  persuasivoM?: Number = 0;
+  persuasivoL?: Number = 0;
+  gentilM?: Number = 0;
+  gentilL?: Number = 0;
+  humildeM?: Number = 0;
+  humildeL?: Number = 0;
+  originalM?: Number = 0;
+  originalL?: Number = 0;
+
+  fuerzavoluntadM?: Number = 0;
+  fuerzavoluntadL?: Number = 0;
+  menteabiertaM?: Number = 0;
+  menteabiertaL?: Number = 0;
+  complacienteM?: Number = 0;
+  complacienteL?: Number = 0;
+  animosoM?: Number = 0;
+  animosoL?: Number = 0;
+
+
+  obedienteM?: Number = 0;
+  obedienteL?: Number = 0;
+  quisquillosoM?: Number = 0;
+  quisquillosoL?: Number = 0;
+  inconquistableM?: Number = 0;
+  inconquistableL?: Number = 0;
+  juguetonM?: Number = 0;
+  juguetonL?: Number = 0;
+
+
+  aventureroM?: Number = 0;
+  aventureroL?: Number = 0;
+  receptivoM?: Number = 0;
+  receptivoL?: Number = 0;
+  cordialM?: Number = 0;
+  cordialL?: Number = 0;
+  moderadoM?: Number = 0;
+  moderadoL?: Number = 0;
+
   appPaterno?: string;
   appMaterno?: string;
+
   nombre?: string;
   programaInteres?: string;
   otroPrograma?: string = '-';
